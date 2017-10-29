50274eba94e6b6d17609fb1c70c54dd94f2c1f22
sgalutira <sheena.galutira@gmail.com>
2017-10-29 03:00:17 -1000

    tracks some faces

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var myTracker = new tracking.Tracker('target');
-var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
 
-objects.on('track', function(event) {
-  if (event.data.length === 0) {
-    // No objects were detected in this frame.
-  } else {
-    event.data.forEach(function(rect) {
-      // rect.x, rect.y, rect.height, rect.width
-    });
-  }
+var img = document.getElementById('test-image');
+
+var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+tracker.setStepSize(1.7);
+
+tracking.track('#test-image', tracker);
+
+tracker.on('track', function(event) {
+  event.data.forEach(function(rect) {
+    window.plot(rect.x, rect.y, rect.width, rect.height);
+  });
 });
 
-
-
-console.log('tracker', myTracker);
-console.log('objects', objects);
+window.plot = function(x, y, w, h) {
+  var rect = document.createElement('div');
+  document.querySelector('.container').appendChild(rect);
+  rect.classList.add('rect');
+  rect.style.width = w + 'px';
+  rect.style.height = h + 'px';
+  rect.style.left = (img.offsetLeft + x) + 'px';
+  rect.style.top = (img.offsetTop + y) + 'px';
+};
